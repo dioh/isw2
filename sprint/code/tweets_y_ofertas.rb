cdd2391317c0aa1eef53d8776667a1d99515b698
danito <danito@manzi.(none)>
2013-04-28 20:23:36 -0300

    agregamos cosas de modelo y borre directorios

---
+++ sprint/code/tweets_y_ofertas.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/ruby
+
+class Tweet
+    def initialize ; end 
+end
+
+class ExtractorDeOfertas
+    def initialize(); end
+
+    def obtener oferta; end
+
+end
+
+class Oferta
+    def initialize(prod, precio, lugar)
+        @producto   = prod
+        @precio     = precio
+        @lugar      = lugar
+    end 
+end
+
+class Producto; end
+
+class Precio; end
+
+
