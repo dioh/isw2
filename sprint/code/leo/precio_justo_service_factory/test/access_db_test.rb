5fa5abb7138d2349879e259e38c83e5a82a25b58
Leonardo M. Tilli <leotilli@gmail.com>
2013-05-12 18:11:39 -0300

    DB Concurrence test

--- sprint/code/leo/precio_justo_service_factory/test/access_db_test.rb
+++ sprint/code/leo/precio_justo_service_factory/test/access_db_test.rb
@@ -3,8 +3,11 @@ require '../lib/sqlite3/sqlite3_db_connection_manager_proxy'
 if __FILE__ == $PROGRAM_NAME
   connManager = Sqlite3DBConnectionManagerProxy.instance()
   db_connection = connManager.connection('./precio_justo.db')
-  results = db_connection.execute('select * from oferta;')
-  results.each { |row|
-    puts row['producto'] + ',' + row['lugar']
-  }
+  while true
+    results = db_connection.execute('select * from oferta;')
+    results.each { |row|
+      puts row['producto'] + ',' + row['lugar']
+    }
+    sleep(10)
+  end
 end
